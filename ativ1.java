8869829ad5d3c0e532f67812eb3161e5692696fb
Williamifc07 <willwarmling17@gmail.com>
2025-03-19 11:52:26 -0300

    Add files via upload

---
+++ ativ1.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        /*int numero;
+        long numero2;
+        Double numero3;
+        Float numero4;
+        char
+        boolean
+        String nome = 'william';
+        System.out.println("Hello, World!");*/
+      
+      //1
+      
+      float altura = 15;
+      float largura = 20;
+      
+      float terreno = altura * largura;
+      
+      System.out.println(terreno);
+      
+      //2
+      
+      int cavalo = 12;
+      int ferraduras = cavalo * 4;
+      System.out.println(ferraduras);
+      
+      //3
+      
+      String nome = "william";
+      int idade = 17;
+      int dias = idade * 365;
+      System.out.println(dias);
+      
+      //4
+      
+      
+  }
+}

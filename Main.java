0d5877461a6ef8afae691443c298b1c97a42ca86
LuanBuzzi <86968009+LuanBuzzi@users.noreply.github.com>
2025-03-19 11:30:46 -0300

    Add files via upload
    Diferenças do Java - Primeiro contato

---
+++ Main.java
@@ -0,0 +1,90 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+     
+     /* 
+      int numero;
+      long numero2;
+      double numero3;
+      float numero4;
+      char 
+      boolean 
+      
+      String nome = "Luan"; 
+      */
+      
+      int num1 = 2;
+      int num2 = 2;
+      int soma = num1 + num2;
+  
+      ==
+      !=
+      >
+      <
+      >=
+      <=
+      
+      &&
+      ||
+      !
+      
+      if(num1 == num2){
+        
+      } else if(num1 != num2){
+        
+      } else {
+        
+      }
+      
+      switch(num1){
+        case 1:
+          System.out.println("é 1");
+        case 2:
+          System.out.println("é 2");
+          break
+        default:
+          System.out.println("É outra coisa");
+      }
+  
+      
+      for(int = 0; i < 10; i++){
+        System.out.println(i);
+      }
+      
+      while(num1 <= 10){
+        
+      }
+      do{
+        
+      }while(num1<=10);
+      
+      public void calculaNumero(int num1, int num2){
+        int soma = num1 + num2;
+        System.out.println(soma);
+      }
+      
+      public int calculaNumero(int num1, int num2){
+        int soma = num1 + num2;
+        System.out.println(soma);
+        return soma;
+      }
+      
+      int[] vetor = new int[10]
+      int[] matriz = new int[10][10]
+      
+      System.out.println(matriz[5][3]);
+      for(int i = 0; i < matriz.length; i++){
+        
+      }
+      
+      int num = Integer.parseInt(texto);
+      float num2 = Float.parseFloat();
+      num.toString();
+
+      
+      System.out.println(soma);
+    
+      System.out.println("Hello, World!");
+  }
+}
